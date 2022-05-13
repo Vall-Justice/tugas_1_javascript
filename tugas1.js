6e8af5e720a835c332c1a0d7a6660cbd4f3d3724
Vall-Justice <104562832+Vall-Justice@users.noreply.github.com>
2022-05-13 23:56:17 +0700

    Tugas 1 javascript

---
+++ tugas1.js
@@ -0,0 +1,9 @@
+ // tugas 1 js
+          
+var namaDepan = "Nouval";
+let namaBelakang = "Aji Satrio";
+const umur = 18;
+          
+ console.log(namaDepan);
+ console.log(namaBelakang);
+ console.log(umur);
